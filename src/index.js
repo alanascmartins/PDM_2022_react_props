4405479ed32e9b82f48559ed467dea2df9f7bb6d
Alana Martins <alanascmartins@gmail.com>
2022-09-02 21:42:00 -0300

    apostila 04: bloco de codigo 2.5.5 concluido

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+    return (
+        <div className="container border mt-2">
+            <div className="row border-bottom m-2">
+                <h1 className="display-5 text-center">Seus Pedidos</h1>
+            </div>
+            <div className="row">
+                <div className="col-sm-8 col-md-6">
+                    {/*Cartão dp Bootstrap*/}
+                    <div className="card my-2">{/* my-2 descolou o card da borda*/}
+                        <div className="card-header text-muted">22/04/2021</div>
+                        {/*Corpo do Cartão*/}
+                        <div className="card-body d-flex">
+                            <div className="d-flex align-items-center">
+                            <i className="fas fa-hdd fa-2x"></i>
+                            </div>
+                            {/* flex-grow 1: tomar espaço remanescente */}
+                            <div className="flex-grow-1 ms-2 border">
+                                <h4 className="text-center">SSD</h4>
+                                <p className="text-center">SSD Kingston A400 - SATA</p>
+                            </div> 
+                        </div>    
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+} 
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
